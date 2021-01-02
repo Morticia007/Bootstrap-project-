60d90ca5ed5baac6f3fc58fac0c0c9aa7b49df18
Morticia007 <jacquelyn07@mac.com>
2021-01-02 12:58:18 -0600

    modal control w/iQuery

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,14 @@ $(function() {
         }
     });
 });
+
+
+
+$("#reserveButton").click(function() {
+    $("#reserveModal").modal('show');
+});
+
+
+$("#loginButton").click(function() {
+    $("#loginModal").modal('show');
+});
